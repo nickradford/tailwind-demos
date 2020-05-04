a95ed5009eda38b16aeec12bd8c0cbf94475aba1
Nick Radford <nick@nicholasradford.com>
2020-05-04 11:21:43 -0700

    apple example

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      display: ['"SF Pro Display"', '"Helvetica Neue"', "Helvetica"],
+      text: ['"SF Pro Text"'],
+    },
+  },
+  variants: {
+    opacity: ["group-hover"],
+    boxShadow: ["responsive", "hover", "focus", "active", "group-hover"],
+  },
+  plugins: [],
+};
