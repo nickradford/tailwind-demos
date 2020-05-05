9b2aa908598f7cf996a17ede8e1f8d91e6b43279
Nick Radford <nick@nicholasradford.com>
2020-05-04 21:13:48 -0700

    better responsive behavior

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      transitionDelay: {
+        "250": "250ms",
+        "350": "350ms",
+        "400": "400ms",
+      },
+    },
     fontFamily: {
       display: ['"SF Pro Display"', '"Helvetica Neue"', "Helvetica"],
       text: ['"SF Pro Text"'],
